84b1353f035492464f48222c0f8a500be2397608
smeesh026 <70630030+smeesh026@users.noreply.github.com>
2020-10-01 11:52:25 +0530

    Upload predicted information to firebase cloud firestore

--- app.py
+++ app.py
@@ -19,9 +19,23 @@ from image_processing import preprocess_image
 from custom_model import CustomModel
 from custom_model_with_location import CustomModelLoc
 
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+
+from google.cloud.firestore_v1 import Increment
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Initialize firebase
+cred = credentials.Certificate(
+    "firebase.json")
+firebase_admin.initialize_app(cred)
+
+store = firestore.client()
+snake_species_ref = store.collection(u'snake-species')
+authority_data_ref = store.collection(u'authority-data')
 
 class_index = json.load(open('class_index.json'))
 
@@ -55,8 +69,9 @@ image_transforms = transforms.Compose([transforms.Resize((img_size, img_size)),
                                        ])
 
 
-def get_species_info(predicted_idx):
-    return class_index[predicted_idx]
+def get_species_info(class_id):
+    snake_species = snake_species_ref.document(class_id).get()
+    return snake_species.to_dict()
 
 
 def transform_image(preprocessed_image):
@@ -65,6 +80,12 @@ def transform_image(preprocessed_image):
         return image_transforms(proc_img).unsqueeze(0)
     except:
         return {"message": "A Transform error has occurred"}
+
+
+def get_add_data(form):
+    values = [float(form['lat']), float(form['lng']),
+              int(form['hour']), int(form['minute'])]
+    return np.array([values])
 
 
 def get_prediction(image_bytes, add_data=None):
@@ -98,6 +119,38 @@ def get_prediction(image_bytes, add_data=None):
         return {"message": "A Prediction error has occurred"}
 
 
+def increment_sighting(class_id):
+    authority_data_ref.document(class_id).update({
+        u'y': Increment(1)
+    })
+
+
+def save_to_firestore_norm(user_id, classification, lat, lng, current_time):
+    data = {
+        u'time': current_time,
+        u'user': user_id,
+        u'classId': classification["classId"],
+        u'confidence': classification["confidence"],
+        u'showLoc': True,
+        u'location': {
+            u'lat': lat,
+            u'lng': lng
+        }
+    }
+    store.collection("sightings").document(current_time).set(data)
+    increment_sighting(classification["classId"])
+
+
+def save_to_firestore(user_id, classification, current_time):
+    data = {
+        u'time': current_time,
+        u'user': user_id,
+        u'classId': classification["classId"],
+        u'confidence': classification["confidence"],
+    }
+    store.collection("sightings").document(current_time).set(data)
+
+
 @app.route('/')
 @cross_origin()
 def hello():
@@ -111,6 +164,7 @@ def predict():
         current_time = time.strftime("%Y-%m-%d-%H-%M-%S")
         img_file = request.files['file']
         filename = img_file.mimetype
+        user_id = request.form['user']
         if "image" in filename or "octet-stream" in filename:
             img_bytes = cv2.imdecode(np.frombuffer(
                 img_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
@@ -125,12 +179,17 @@ def predict():
             values = get_prediction(preprocessed_image, add_data)
             if type(values) == dict and "classId" in values:
                 species_val = get_species_info(values["classId"])
+                if "lat" in request.form:
+                    save_to_firestore_norm(
+                        user_id, values, request.form['lat'], request.form['lng'], current_time)
+                else:
+                    save_to_firestore(user_id, values, current_time)
 
                 return jsonify({'data': {
                     **species_val,
                     "confidence": values["confidence"],
                     "standardDev": values["standardDev"],
-                    "time": current_time,
+                    "time": current_time
                 }}), 200
             else:
                 return jsonify({"error": values}), 400
